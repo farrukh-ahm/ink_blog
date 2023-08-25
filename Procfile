d9f761fbf1ac2e2c8701f09791e97de627b5e748
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2023-08-25 20:48:45 +0000

    Install packages and create new project and app. Wire up database, cloudinary, static files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ink_blog.wsgi
---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault('DATABASE_URL', 'postgres://zhzhnzux:bRuGKvM_lPWh3a_BEyUOIHrJI2xzDE-p@trumpet.db.elephantsql.com/zhzhnzux')
+os.environ.setdefault('CLOUDINARY_URL', 'cloudinary://265487814213721:owt3PnJbXJMuba6ff8x3MEjigBc@farrukh-ahm')
+os.environ.setdefault('SECRET_KEY', 'AntecedentInk')
