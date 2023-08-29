e77dbc5b557d07a4d84943ea06946d4d36b62f2b
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2023-08-29 15:27:03 +0000

    Wire up materliasecss

--- ink/views.py
+++ ink/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.contrib import messages
+from django.views import generic, View
+from .models import *
+from django.utils.text import slugify
 
-# Create your views here.
+class Home(View):
+
+    def get(self, request, *args, **kwargs):
+
+        return render(request, 'index.html')
---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    console.log("connected")
+    // NAVBAR INIT
+    var navbar = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(navbar);
+  });
