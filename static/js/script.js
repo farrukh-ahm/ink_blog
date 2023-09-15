30614e3fdf71062385ce7029a5af863ceb2b6271
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2023-09-15 14:33:00 +0000

    Customising comment area and trying to implement modal

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-    console.log("connected")
-    
+    console.log("connected, hello")
+    console.log("here")
     // NAVBAR INIT
     var navbar = document.querySelectorAll('.sidenav');
     M.Sidenav.init(navbar);
@@ -8,5 +8,22 @@ document.addEventListener('DOMContentLoaded', ()=> {
     // SELECT INIT
     let selectInt = document.querySelectorAll('select');
     M.FormSelect.init(selectInt);
+
+    // MODAL TRIGGER
+    // let elems = document.querySelectorAll('.modal');
+    // M.Modal.init(elems);
+    let popOp = document.querySelector('.modal');
+    let modal = M.Modal.init(popOp);
+    let modalTrigger = document.querySelector(".modal-trigger")
+    let modalClose = document.querySelector(".modal-close")
+    modalTrigger.addEventListener("click", ()=>{
+      console.log("click")
+      modal.open()
+    })
+    for(let close of modalClose){
+     close.addEventListener("click", ()=>{
+        modal.close()
+    })
+    }
 
   });
